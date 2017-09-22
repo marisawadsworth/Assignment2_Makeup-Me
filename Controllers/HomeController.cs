1146cc7413e9fba2aaaa2db59e2bec8033f6a9b8
Marisa Wadsworth <marisawadsworth@Marisas-MacBook-Air.local>
2017-09-22 14:37:22 +1200

    initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace assesment2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Portfolio()
+        {
+            ViewData["Message"] = "Your portfolio page.";
+
+            return View();
+        }
+
+        public IActionResult Team()
+        {
+            ViewData["Message"] = "Your team page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
