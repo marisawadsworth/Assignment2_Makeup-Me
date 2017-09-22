f91d411876fa99f9317a7520969fdbaa14779f12
Marisa Wadsworth <marisawadsworth@Marisas-MacBook-Air.local>
2017-09-22 16:07:43 +1200

    inital commit

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,29 +15,21 @@ namespace assesment2.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
-
             return View();
         }
 
         public IActionResult Portfolio()
         {
-            ViewData["Message"] = "Your portfolio page.";
-
             return View();
         }
 
         public IActionResult Team()
         {
-            ViewData["Message"] = "Your team page.";
-
             return View();
         }
 
         public IActionResult Contact()
         {
-            ViewData["Message"] = "Your contact page.";
-
             return View();
         }
 
